dbe3f3f176d5ad297ad197866e4affac6f0c7bbf
contafdois-beep <contafdois@gmail.com>
2025-10-09 11:06:12 -0300

    Resolvendo conflitos

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
+// Carousel
 const track = document.querySelector(".carousel-track");
 const prevBtn = document.querySelector(".btn.prev");
 const nextBtn = document.querySelector(".btn.next");
-const slides = document.querySelectorAll(".carouselSlide");
+const slides = document.querySelectorAll(".carousel-slide");
 
 let index = 0;
+
+function updateCarousel() {
+  const slideWidth = slides[0].offsetWidth;
+  track.style.transform = `translateX(${-index * slideWidth}px)`;
+}
 
 nextBtn.addEventListener("click", () => {
   if (index < slides.length - 1) {
@@ -19,9 +25,23 @@ prevBtn.addEventListener("click", () => {
   }
 });
 
-function updateCarousel() {
-  const slideWidth = slides[0].offsetWidth;
-  track.style.transform = `translateX(${-index * slideWidth}px)`;
-}
+/* copiar */
+      const botoes = document.querySelectorAll(".copyBtn");
+const toast = document.getElementById('toast');
 
-/* =============================== */
+  function mostrarToast(mensagem) {
+    toast.textContent = mensagem;
+    toast.classList.add('show');
+
+    // Esconde após 2,5s
+    setTimeout(() => toast.classList.remove('show'), 2500);
+  }
+
+  botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+      const texto = botao.dataset.text;
+      navigator.clipboard.writeText(texto)
+        .then(() => mostrarToast('Código copiado!'))
+        .catch(err => console.error('Erro ao copiar:', err));
+    });
+  });
